c2f6ac7688e04910e4f802accabd653a121ecc6b
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-12-22 22:17:31 +0300

    Add BaseEntity, IBaseEntity.

---
+++ Core/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Entities
+{
+    public class BaseEntity : IBaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        
+
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+
+        }
+    }
+}
---
+++ Core/Entities/IBaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities
+{
+    public interface IBaseEntity
+    {
+        Guid Id { get; set; }
+        DateTime CreatedDate { get; set; }
+        DateTime? DeletedDate { get; set; }
+        DateTime? UpdatedDate { get; set; }
+    }
+}
