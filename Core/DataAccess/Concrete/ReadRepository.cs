357638e6fdf927262f010b7e2a5df74e52864d36
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-12-24 10:52:28 +0300

    Add ReadRepository, WriteRepository.

---
+++ Core/DataAccess/Concrete/ReadRepository.cs
@@ -0,0 +1,37 @@
+﻿using Core.DataAccess.Abstract;
+using Core.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Core.DataAccess.Concrete
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : BaseEntity
+    {
+        private readonly DbContext _dbContext;
+        public ReadRepository(DbContext dbContext)
+        {
+            _dbContext=dbContext;
+        }
+        public DbSet<T> Table => _dbContext.Set<T>();
+
+        public Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FirstorDefaultAsnc(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/DataAccess/Concrete/WriteRepository.cs
@@ -0,0 +1,37 @@
+﻿using Core.DataAccess.Abstract;
+using Core.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.DataAccess.Concrete
+{
+    public class WriteRepository<T> : IWriteRepository<T> where T : BaseEntity
+    {
+        private readonly DbContext _dbContext;
+        public WriteRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public DbSet<T> Table => _dbContext.Set<T>();
+
+        public Task<T> CreateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> DeleteByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
