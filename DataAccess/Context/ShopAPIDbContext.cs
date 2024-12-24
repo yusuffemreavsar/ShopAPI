2c430162e27237e198dda70260810f4a18225058
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-12-24 11:01:32 +0300

    Add ShopAPIDbContext.

---
+++ DataAccess/Context/ShopAPIDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Context
+{
+    public class ShopAPIDbContext : DbContext
+    {
+        public ShopAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<User> Users { get; set; }
+        DbSet<OperationClaim> OperationClaims { get; set; }
+        DbSet<UserOperationClaim> UserOperations { get; set; }
+    }
+}
